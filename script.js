172b696fb7f439e86268eea15ace91115651ad59
Jordan <jordalex2018@icloud.com>
2020-07-17 00:10:59 +0100

    Add some new images and update website to add new features

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 ﻿console.log("hello world");
+
+const url = 'https://cat-fact.herokuapp.com/facts/random'
+function fetch_fact() {
+  $.get(url, function(data, status) {
+    console.log(data);
+  });
+}
