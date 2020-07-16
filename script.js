4c8701f9edb007d40f2956730c2e9fbb072ae41b
Jordan <jordalex2018@icloud.com>
2020-07-17 00:12:23 +0100

    Update button to change languages and other changes

--- script.js
+++ script.js
@@ -4,5 +4,20 @@ const url = 'https://cat-fact.herokuapp.com/facts/random'
 function fetch_fact() {
   $.get(url, function(data, status) {
     console.log(data);
+    display_fact(data);
   });
 }
+
+function display_fact(data) {
+  $(".Section");
+  console.log($(".Section-Lang").removeAttr("hidden"));
+  console.log("test");
+}
+
+function change_lang(data) {
+  $("[name='cur_language']")[0].textContent = data.textContent;
+}
+
+$( document ).ready(function() {
+  console.log("ready!");
+});
